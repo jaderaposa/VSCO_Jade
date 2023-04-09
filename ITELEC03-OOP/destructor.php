417ed550a7ed651ce700d40c7667cd44722fa4f3
jaderaposa <jaduya678@gmail.com>
2023-04-09 23:35:00 +0800

    OOP

--- ITELEC03-OOP/destructor.php
+++ ITELEC03-OOP/destructor.php
@@ -1,35 +1,21 @@
 <?php
 
-class Logger {
-  public function __construct() {
-    // Open a log file for writing
-    $this->fileHandle = fopen('log.txt', 'a');
-  }
+class MyClass {
+    private $name;
 
-  public function writeLog($message) {
-    // Write the message to the log file
-    fwrite($this->fileHandle, $message . "\n");
-  }
+    public function __construct($name) {
+        $this->name = $name;
+        echo "Creating object with name: " . $this->name . "<br>";
+    }
 
-  public function __destruct() {
-    // Close the log file
-    fclose($this->fileHandle);
-  }
+    public function __destruct() {
+        echo "Destroying object with name: " . $this->name . "<br>";
+    }
 }
 
-// Create a new logger object
-$logger = new Logger();
+$obj1 = new MyClass("Object 1");
+$obj2 = new MyClass("Object 2");
+$obj3 = new MyClass("Object 3");
 
-// Write some logs
-$logger->writeLog("An error occurred!");
-$logger->writeLog("User logged in");
-
-// The logger object will be automatically destroyed at the end of the script
-
+unset($obj1);
 ?>
-
-<!-- In this example, we have a Logger class which is responsible for logging messages to a file. We have a constructor method __construct which opens a log file for writing and a destructor method __destruct which closes the log file.
-
-We also have a writeLog method which takes a message parameter and writes it to the log file.
-
-Finally, we create a Logger object and use it to write some logs. At the end of the script, the Logger object will be automatically destroyed and the __destruct method will be called, which will close the log file. -->
