c16c930eb52f1309115bb96d5761fe137385fcec
jaderaposa <jaduya678@gmail.com>
2023-04-23 12:40:14 +0800

    registration form

--- ITPC222/Prefinals-Practical-Exam/index.php
+++ ITPC222/Prefinals-Practical-Exam/index.php
@@ -70,7 +70,23 @@ include('db_config.php');
         <hr>
 
         <div>
-          
+          <div>
+            <form>
+              <label for="name">Name:</label>
+              <input type="text" id="name" name="name"><br>
+
+              <label for="username">Username:</label>
+              <input type="text" id="username" name="username"><br>
+
+              <label for="password">Password:</label>
+              <input type="password" id="password" name="password"><br>
+
+              <label for="contact-number">Contact Number:</label>
+              <input type="tel" id="contact-number" name="contact-number"><br>
+
+              <input type="submit" value="Submit">
+            </form>
+          </div>
         </div>
       </div>
     
