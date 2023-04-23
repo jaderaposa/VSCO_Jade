8d2b5d92cb036082fb3d72628292a48d2bc861d0
jaderaposa <jaduya678@gmail.com>
2023-04-23 12:23:40 +0800

    big improvement

--- ITPC222/Prefinals-Practical-Exam/index.php
+++ ITPC222/Prefinals-Practical-Exam/index.php
@@ -40,11 +40,40 @@ include('db_config.php');
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Contact</a></li>
+            <li><a href="index.php"  class="active">Registration Form</a></li>
           </ul>
         </nav>
+        <script>
+          // Get the current URL
+          var currentUrl = window.location.href;
+
+          // Loop through each link in the navigation menu
+          $('nav ul li a').each(function() {
+
+            // If the link's URL matches the current URL, add the "active" class
+            if ($(this).attr('href') === currentUrl) {
+              $(this).addClass('active');
+            }
+
+          });
+        </script>
       </div>
         
     </div>
+
+    
+      <div class="container">
+        <div class="container-title">
+        <h1>Registration Form</h1>
+        </div>
+
+        <hr>
+
+        <div>
+          
+        </div>
+      </div>
+    
 
 
 
