b829d7a01e421150bd13c9775ab921a6f2037827
Glaiza <milletemea@gmail.com>
2025-03-13 13:30:19 +0800

    Update theme_template.js

--- robbowen.synthwave-vscode-0.1.15/src/js/theme_template.js
+++ robbowen.synthwave-vscode-0.1.15/src/js/theme_template.js
@@ -1,8 +1,8 @@
 (function () {
-	//====================================
-	// Theme replacement CSS (Glow styles)
-	//====================================
-	const tokenReplacements = {
+  //====================================
+  // Theme replacement CSS (Glow styles)
+  //====================================
+  const tokenReplacements = {
 		/* Red */
 		fe4450: "color: #fff5f6; text-shadow: 0 0 2px #000, 0 0 10px #fc1f2cFF, 0 0 5px #fc1f2cFF, 0 0 25px #fc1f2cFF; backface-visibility: hidden;",
 		/* Neon pink */
@@ -17,80 +17,82 @@
 		"10cee7": "color: #10cee7; text-shadow: 0 0 2px #001716, 0 0 3px #03edf9FF, 0 0 5px #03edf9FF, 0 0 8px #03edf9FF; backface-visibility: hidden;",
 	};
 
-	//=============================
-	// Helper functions
-	//=============================
+  //=============================
+  // Helper functions
+  //=============================
 
-	/**
-	 * @summary Check if the style element exists and that it has synthwave '84 color content
-	 * @param {HTMLElement} tokensEl the style tag
-	 * @param {object} replacements key/value pairs of colour hex and the glow styles to replace them with
-	 * @returns {boolean}
-	 */
-	const themeStylesExist = (tokensEl, replacements) => {
-		return (
-			tokensEl.innerText !== "" &&
-			Object.keys(replacements).every((color) => {
-				return tokensEl.innerText.toLowerCase().includes(`#${color}`);
-			})
-		);
-	};
+  /**
+   * @summary Check if the style element exists and that it has synthwave '84 color content
+   * @param {HTMLElement} tokensEl the style tag
+   * @param {object} replacements key/value pairs of colour hex and the glow styles to replace them with
+   * @returns {boolean}
+   */
+  const themeStylesExist = (tokensEl, replacements) => {
+    return tokensEl.innerText !== '' && 
+      Object.keys(replacements).every(color => {
+        return tokensEl.innerText.toLowerCase().includes(`#${color}`);
+      });
+  };
 
-	/**
-	 * @summary Search and replace colours within a CSS definition
-	 * @param {string} styles the text content of the style tag
-	 * @param {object} replacements key/value pairs of colour hex and the glow styles to replace them with
-	 * @returns
-	 */
-	const replaceTokens = (styles, replacements) =>
-		Object.keys(replacements).reduce((acc, color) => {
-			const re = new RegExp(`color: #${color};`, "gi");
-			return acc.replace(re, replacements[color]);
-		}, styles);
+  /**
+   * @summary Search and replace colours within a CSS definition
+   * @param {string} styles the text content of the style tag
+   * @param {object} replacements key/value pairs of colour hex and the glow styles to replace them with
+   * @returns 
+   */
+  const replaceTokens = (styles, replacements) => Object.keys(replacements).reduce((acc, color) => {
+    const re = new RegExp(`color: #${color};`, 'gi');
+    return acc.replace(re, replacements[color]);
+  }, styles);
 
-	/**
-	 * @summary Checks if a theme is applied, and that the theme belongs to the Synthwave 84 family
-	 * @returns {boolean}
-	 */
-	const usingSynthwave = () => {
-		const appliedTheme = document.querySelector('[class*="theme-json"]');
-		const synthWaveTheme = document.querySelector('[class*="RobbOwen-synthwave-vscode-themes"]');
-		return appliedTheme && synthWaveTheme;
-	};
+  /**
+   * @summary Checks if a theme is applied, and that the theme belongs to the Synthwave 84 family
+   * @returns {boolean}
+   */
+  const usingSynthwave = () => {
+    const appliedTheme = document.querySelector('[class*="theme-json"]');
+    const synthWaveTheme = document.querySelector('[class*="RobbOwen-synthwave-vscode-themes"]');
+    return appliedTheme && synthWaveTheme;
+  }
 
-	/**
-	 * @summary Checks if the theme is synthwave, and that the styles exist, ready for replacement
-	 * @param {HTMLElement} tokensEl the style tag
-	 * @param {object} replacements key/value pairs of colour hex and the glow styles to replace them with
-	 * @returns
-	 */
-	const readyForReplacement = (tokensEl, tokenReplacements) =>
-		tokensEl
-			? // only init if we're using a Synthwave 84 subtheme
-			  usingSynthwave() &&
-			  // does it have content ?
-			  themeStylesExist(tokensEl, tokenReplacements)
-			: false;
+  /**
+   * @summary Checks if the theme is synthwave, and that the styles exist, ready for replacement
+   * @param {HTMLElement} tokensEl the style tag
+   * @param {object} replacements key/value pairs of colour hex and the glow styles to replace them with
+   * @returns 
+   */
+  const readyForReplacement = (tokensEl, tokenReplacements) => tokensEl 
+    ? (
+      // only init if we're using a Synthwave 84 subtheme
+      usingSynthwave() &&         
+      // does it have content ?
+      themeStylesExist(tokensEl, tokenReplacements)
+    )
+    : false;
 
-	/**
-	 * @summary Attempts to bootstrap the theme
-	 * @param {boolean} disableGlow
-	 * @param {MutationObserver} obs
-	 */
-	const initNeonDreams = (disableGlow, obs) => {
-		const tokensEl = document.querySelector(".vscode-tokens-styles");
+  /**
+   * @summary Attempts to bootstrap the theme
+   * @param {boolean} disableGlow 
+   * @param {MutationObserver} obs 
+   */
+  const initNeonDreams = (disableGlow, obs) => {
+    const tokensEl = document.querySelector('.vscode-tokens-styles');
 
-		if (!tokensEl || !readyForReplacement(tokensEl, tokenReplacements)) {
-			return;
-		}
+    if (!tokensEl || !readyForReplacement(tokensEl, tokenReplacements)) {
+      return;
+    }
 
-		const initialThemeStyles = tokensEl.innerText;
-
-		// Replace tokens with glow styles
-		let updatedThemeStyles = !disableGlow ? replaceTokens(initialThemeStyles, tokenReplacements) : initialThemeStyles;
-
-		/* append the remaining styles */
-		updatedThemeStyles = `${updatedThemeStyles}.monaco-editor .margin, .monaco-editor .inputarea.ime-input {
+    // Add the theme styles if they don't already exist in the DOM
+    if (!document.querySelector('#synthwave-84-theme-styles')) {
+      const initialThemeStyles = tokensEl.innerText;
+      
+      // Replace tokens with glow styles
+      let updatedThemeStyles = !disableGlow 
+        ? replaceTokens(initialThemeStyles, tokenReplacements) 
+        : initialThemeStyles;
+      
+      /* append the remaining styles */
+      updatedThemeStyles = `${updatedThemeStyles}.monaco-editor .margin, .monaco-editor .inputarea.ime-input {
       background: transparent !important;
     }
     
@@ -195,57 +197,53 @@
   display: inline-block
 }
 `;
+  
+      const newStyleTag = document.createElement('style');
+      newStyleTag.setAttribute("id", "synthwave-84-theme-styles");
+      newStyleTag.innerText = updatedThemeStyles.replace(/(\r\n|\n|\r)/gm, '');
+      document.body.appendChild(newStyleTag);
+      
+      console.log('Synthwave \'84: NEON DREAMS initialised!');
+    }
 
-		const newStyleTag = document.createElement("style");
-		newStyleTag.setAttribute("id", "synthwave-84-theme-styles");
-		newStyleTag.innerText = updatedThemeStyles.replace(/(\r\n|\n|\r)/gm, "");
-		document.body.appendChild(newStyleTag);
+    // disconnect the observer because we don't need it anymore
+    if (obs) {
+      obs.disconnect();
+      obs = null;
+    }
+  };
 
-		console.log("Synthwave '84: NEON DREAMS initialised!");
+  /**
+   * @summary A MutationObserver callback that attempts to bootstrap the theme and assigns a retry attempt if it fails
+   */
+  const watchForBootstrap = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+      if (mutation.type === 'attributes' || mutation.type === 'childList') {
+        // does the style div exist yet?
+        const tokensEl = document.querySelector('.vscode-tokens-styles');
+        if (readyForReplacement(tokensEl, tokenReplacements)) {
+          // If everything we need is ready, then initialise
+          initNeonDreams([DISABLE_GLOW], observer);
+        } else {
+          if (tokensEl) {
+            // sometimes VS code takes a while to init the styles content, so if there stop this observer and add an observer for that
+            observer.disconnect();
+            observer.observe(tokensEl, { childList: true });
+          }
+        }
+      }
+    }
+  };
 
-		// disconnect the observer because we don't need it anymore
-		if (obs) {
-			obs.disconnect();
-			obs = null;
-		}
-	};
-
-	/**
-	 * @summary A MutationObserver callback that attempts to bootstrap the theme and assigns a retry attempt if it fails
-	 */
-	const watchForBootstrap = function (mutationsList, observer) {
-		for (let mutation of mutationsList) {
-			if (mutation.type === "attributes") {
-				// does the style div exist yet?
-				const tokensEl = document.querySelector(".vscode-tokens-styles");
-				if (readyForReplacement(tokensEl, tokenReplacements)) {
-					// If everything we need is ready, then initialise
-					initNeonDreams([DISABLE_GLOW], observer);
-				} else {
-					// sometimes VS code takes a while to init the styles content, so if there stop this observer and add an observer for that
-					observer.disconnect();
-					observer.observe(tokensEl, { childList: true });
-				}
-			}
-			if (mutation.type === "childList") {
-				const tokensEl = document.querySelector(".vscode-tokens-styles");
-				if (readyForReplacement(tokensEl, tokenReplacements)) {
-					// Everything we need should be ready now, so initialise
-					initNeonDreams([DISABLE_GLOW], observer);
-				}
-			}
-		}
-	};
-
-	//=============================
-	// Start bootstrapping!
-	//=============================
-	  setTimeout(() => {
-			initNeonDreams([DISABLE_GLOW], observer);
-		}, 5000);
-	// Grab body node
-	const bodyNode = document.querySelector("body");
-	// Use a mutation observer to check when we can bootstrap the theme
-	const observer = new MutationObserver(watchForBootstrap);
-	observer.observe(bodyNode, { attributes: true });
+  //=============================
+  // Start bootstrapping!
+  //=============================
+  // Grab body node
+  const bodyNode = document.querySelector('body');
+  // Use a mutation observer to check when we can bootstrap the theme
+  const observer = new MutationObserver(watchForBootstrap);
+  /* watch for both attribute and childList changes because, depending on 
+  the VS code version, the mutations might happen on the body, or they might 
+  happen on a nested div */
+  observer.observe(bodyNode, { attributes: true, childList: true });
 })();
